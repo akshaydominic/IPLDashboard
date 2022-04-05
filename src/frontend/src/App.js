7d3f0250949e579bd991b8fc14ba240334c59545
Akshay <adominic21@gmail.com>
2022-04-06 01:32:18 +0530

    Links and Routes added

--- src/frontend/src/App.js
+++ src/frontend/src/App.js
@@ -1,10 +1,18 @@
 import './App.css';
+import { Routes, Route } from 'react-router-dom';
+import { Router } from 'react-router-dom';
 import { TeamPage } from './pages/TeamPage';
 
 function App() {
 	return (
 		<div className="App">
-			<TeamPage />
+			<Routes>
+				<Route path="/">
+					<Route path="teams">
+						<Route path=":teamName" element={<TeamPage />} />
+					</Route>
+				</Route>
+			</Routes>
 		</div>
 	);
 }
